bb784464654b60d65ca7f950de7dff4ea5383533
Maxwell Cochrane <71269626+MJC1994@users.noreply.github.com>
2023-02-06 20:51:08 +0000

    0.2

--- src/App.js
+++ src/App.js
@@ -1,173 +1,183 @@
 import { useEffect, useState } from "react";
-import riddles from './riddles.json';
+
+
+import riddles from "./riddles.json";
 import "./App.css";
 
-
-import "./Fonts/Nunito-Black.ttf"
-import "./Fonts/Nunito-Regular.ttf"
-import "./Fonts/Nunito-Medium.ttf"
-import "./Fonts/Nunito-Bold.ttf"
-
+import "./Fonts/Nunito-Black.ttf";
+import "./Fonts/Nunito-Regular.ttf";
+import "./Fonts/Nunito-Medium.ttf";
+import "./Fonts/Nunito-Bold.ttf";
 
 function App() {
-  const [solution, setSolution] = useState('')
+  const [solution, setSolution] = useState("");
   const [guesses, setGuesses] = useState(Array(3).fill(null));
-  const [currentGuess, setCurrentGuess] = useState('');
-  const [isGameOver,  setIsGameOver] = useState(false);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [correct, setCorrect] = useState(false);
+  const [noGuessesLeft, setNoGuessesLeft] = useState(false);
   const [riddle, setRiddle] = useState({});
+  const [startTime] = useState(Date.now());
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  const handleElapsedTime = () =>   {
+      setElapsedTime((Date.now() - startTime) / 1000);
+  }
 
   useEffect(() => {
-    const today = new Date().toISOString().slice(0,10);
-    const currentRiddle = riddles.find(riddle => riddle.DATE === today);
+    const today = new Date().toISOString().slice(0, 10);
+    const currentRiddle = riddles.find((riddle) => riddle.DATE === today);
     setRiddle(currentRiddle);
-    setSolution(currentRiddle.ANSWER.toLocaleLowerCase())
+    setSolution(currentRiddle.ANSWER.toLocaleLowerCase());
   }, [riddle.QUESTION]);
 
-  function handleKeyboard (e) {
-    handleType(e.key)
+  function handleKeyboard(e) {
+    handleType(e.key);
   }
 
-  function guess () {
-    const newGuesses = [...guesses]
-      newGuesses[guesses.findIndex(val => val == null)] = currentGuess;
-      setGuesses(newGuesses)
-      setCurrentGuess('')
-      const isCorrect = solution === currentGuess
-      if(isCorrect) {
-         setIsGameOver(true);
-      }
+  function guess() {
+    const newGuesses = [...guesses];
+    newGuesses[guesses.findIndex((val) => val == null)] = currentGuess;
+    setGuesses(newGuesses);
+    setCurrentGuess("");
+    const isCorrect = solution === currentGuess;
+    if (isCorrect) {
+      setIsGameOver(true);
+      setCorrect(true)
+      handleElapsedTime()
+    }
   }
-
-  function handleType (event)  {
-
-    let x = fetch('../netlify/functions/hello-world/hello-world.js')
-
-
-    if(currentGuess.length === solution.length) {
-      guess()
-    } 
+  function handleType(event) {
+    console.log(startTime)
+    if (guesses.filter(e => e !== null).length === guesses.length) {
+      setIsGameOver(true);
+      setNoGuessesLeft(true);
+    }
     
-    if (event === 'Enter') {
-      if(currentGuess.length  !== solution.length) {
-        return
+    if (event === "Enter") {
+      if (currentGuess.length !== solution.length) {
+        return;
       } else {
-        guess()
-      } 
+        guess();
+      }
     }
-    if(event === 'Backspace'){
-      setCurrentGuess(currentGuess.slice(0, -1))
-      return
+    
+    if (event === "Backspace") {
+      setCurrentGuess(currentGuess.slice(0, -1));
+      return;
     }
-    const isLetter = event.match(/^[a-z]{1}$/)
-    if(isLetter)
-    {
-      setCurrentGuess(oldGuess => oldGuess + event)
-    }
-    if(currentGuess.length !== solution.length) {
-      return
-    }
-    if(currentGuess.length + 1  === solution.length - 1) {
-      guess()
+    
+    const isLetter = event.match(/^[a-z]{1}$/);
+    if (isLetter) {
+      setCurrentGuess((oldGuess) => oldGuess + event);
     }
 
+    if (currentGuess.length === solution.length) {
+      guess();
+    }
+ 
+    if (currentGuess.length !== solution.length) {
+      return;
+    }
+    if (currentGuess.length === solution.length ) {
+      guess();
+    }
   }
-
 
   useEffect(() => {
-    if(isGameOver){
-      return
+    if (isGameOver) {
+      return;
     }
-    window.addEventListener('keydown', handleKeyboard)
-    if ('keydown') {return () => window.removeEventListener('keydown', handleKeyboard)}
-  })
+    window.addEventListener("keydown", handleKeyboard);
+    if ("keydown") {
+      return () => window.removeEventListener("keydown", handleKeyboard);
+    }
+  });
 
   return (
-<div className="app"> 
-    <Title />
-    <section className="game"> 
-        <div className="board">
-          {guesses.map((guess, i) => {
-            const isCurrentGuess =  i === guesses.findIndex(val => val == null)
-              return (
-                <Line 
-                  guess={isCurrentGuess ? currentGuess : guess ?? ''}
-                  isFinal={!isCurrentGuess && guess != null}
-                  solution={solution}
-                  currentGuess={currentGuess}
-                  guesses={guesses}
-                  row={i}
-                  key={i}
-                  />
-              )
-            })
-          }
-        </div>
-      </section>
-    <Riddle q={riddle.QUESTION}/>
-    <Keyboard handleType={handleType}/>
-</div>
+    <div className="app">
+      <Title />
+      {correct ? <Complete solution={solution} elapsedTime={elapsedTime} guesses={guesses} /> : null}
+      {isGameOver ? null 
+      : <div>  
+      <Game guesses={guesses} currentGuess={currentGuess} solution={solution} /> 
+      <Riddle q={riddle.QUESTION} />
+      <Keyboard handleType={handleType} /> 
+      </div>
+      }
+      {noGuessesLeft ? <Idiot /> : null}
+    </div>
   );
 }
 
 export default App;
 
-function Line({guesses, row, guess, isFinal, solution, currentGuess}) {
-  const tiles = []
+function Line({ guesses, row, guess, isFinal, solution, currentGuess }) {
+  const tiles = [];
   for (let i = 0; i < solution.length; i++) {
-    const char = guess[i] 
+    const char = guess[i];
 
-    let className = `tile`
-    
+    let className = `tile`;
 
-
-    if(isFinal) {
-      if (char === solution[i]){
-        className += ' correct'
+    if (isFinal) {
+      if (char === solution[i]) {
+        className += " correct";
       } else if (solution.includes(char)) {
-        className += ' close shake'
+        className += " close shake";
       } else {
-        className += ' incorrect shake'
+        className += " incorrect shake";
       }
     }
 
-    if (currentGuess.length === i && row === guesses.filter(entry => entry !== null).length) {
-      className += ' flash'
+    if (currentGuess.length === i && row === guesses.filter((entry) => entry !== null).length) {
+      className += " flash";
     }
 
-    tiles.push(<div className={className} row={row} col={i} key={i}> {char}</div>)
+    tiles.push(
+      <div className={className} row={row} col={i} key={i}>
+        {" "}
+        {char}
+      </div>
+    );
   }
-  return (
-    <div className="line" > {tiles}</div>
-  )
-} 
-
-
-
-function Riddle({q}) {
-  return (
-    <h4> {q} </h4>
-  )
+  return <div className="line"> {tiles}</div>;
 }
 
+function Riddle({ q }) {
+  return <h4> {q} </h4>;
+}
+
+function Game({ guesses, currentGuess, solution }) {
+  return (
+    <section className="game">
+      <div className="board">
+        {guesses.map((guess, i) => {
+          const isCurrentGuess = i === guesses.findIndex((val) => val == null);
+          return (
+            <Line guess={isCurrentGuess ? currentGuess : guess ?? ""} isFinal={!isCurrentGuess && guess != null} solution={solution} currentGuess={currentGuess} guesses={guesses} row={i} key={i} />
+          );
+        })}
+      </div>
+    </section>
+  );
+}
 
 function Title() {
-  return (
-    <h1> GRIDL</h1>
-  )
+  return <h1> GRIDL</h1>;
 }
 
 function Keyboard({ handleType }) {
-
   const test = (e) => {
-    const { value } = e.target
-    handleType(value)
-  }
+    const { value } = e.target;
+    handleType(value);
+  };
 
   const rowOneKeys = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"];
   const rowTwoKeys = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
   const rowThreeKeys = ["z", "x", "c", "v", "b", "n", "m"];
 
   return (
+    <div className="keyboardContainer">
     <div className="keyboard">
       <section className="rowOne">
         {rowOneKeys.map((value) => (
@@ -186,14 +196,11 @@ function Keyboard({ handleType }) {
       </section>
 
       <section className="rowThree">
-      
-
         {rowThreeKeys.map((value) => (
           <button className="key" key={value} value={value} onClick={test}>
             {value}
           </button>
         ))}
-     
       </section>
       <section className="rowFour">
         <button className="key bigBtn" key="Backspace" value="Backspace" onClick={test}>
@@ -204,5 +211,25 @@ function Keyboard({ handleType }) {
         </button>
       </section>
     </div>
+    </div>
   );
+}
+
+function Complete ({solution, elapsedTime, guesses}) {
+  return (
+    <section className="container">
+        <h2> Correct!</h2>
+        <h4> You got {solution} in {elapsedTime} seconds and {guesses.filter(e => e !== null).length} guesses</h4>
+        <p>Come back tomorrow for a new riddle. or share this with a mate or something...</p>
+      </section>
+  )
+}
+
+function Idiot() {
+  return (
+    <section className="container">
+        <h2> Not today...</h2>
+        <p>Come back tomorrow for a new riddle. or share this with a mate or something...</p>
+      </section>
+  )
 }
---
+++ src/timer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+function Timer() {
+  const [startTime] = useState(Date.now());
+  const [endTime, setEndTime] = useState(null);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  
+
+  const handleStop = () => {
+    setEndTime(Date.now());
+  };
+
+  useEffect(() => {
+    if (startTime && endTime) {
+      setElapsedTime((endTime - startTime) / 1000);
+    }
+  }, [startTime, endTime]);
+
+  return (
+    <div>
+      
+      {startTime && !endTime && <button onClick={handleStop}>Stop</button>}
+      {elapsedTime !== 0 && (
+        <p>Elapsed time: {elapsedTime} seconds</p>
+      )}
+    </div>
+  );
+}
+
+export default Timer;

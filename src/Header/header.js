92368b6d91705f98aabb4e0b74e065b0a01648d9
Maxwell Cochrane <71269626+MJC1994@users.noreply.github.com>
2023-02-12 18:16:34 +0000

    0.9.3 - styling perfection

--- src/Header/header.js
+++ src/Header/header.js
@@ -1,37 +1,31 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import HelpModal from "../modals/help";
 
 const Header = () => {
+  const [showHelpModal, setShowHelpModal] = useState(false);
 
-    const [showHelpModal, setShowHelpModal] = useState(false)
-
-    function handleClick() {
-        setShowHelpModal(showHelpModal => !showHelpModal)
-    } 
+  function handleClick() {
+    setShowHelpModal((showHelpModal) => !showHelpModal);
+  }
 
   return (
     <section className="banner">
-
-    <div className="header mw500 ma"> 
-
-      <h1 className="title noPad"> RIDLr.</h1> 
-        <div className="helpIcon"> 
-              <button className='large' onClick={handleClick}> ℹ️ </button>
+      <div className="header mw500 ma">
+        <h1 className="title noPad"> RIDLr.</h1>
+        <div className="helpIcon">
+          <button className="large" onClick={handleClick}>
+             ℹ️
+          </button>
         </div>
-
-    </div>
-     {showHelpModal ? ( 
-     <div> 
-        <HelpModal /> 
-        <div className="backdrop" onClick={handleClick} /> 
-        
-     </div>
-    )
-     
- : null}
-
-   </section>
-    );
+      </div>
+      {showHelpModal ? (
+        <div>
+          <HelpModal />
+          <div className="backdrop" onClick={handleClick} />
+        </div>
+      ) : null}
+    </section>
+  );
 };
 
 export default Header;
